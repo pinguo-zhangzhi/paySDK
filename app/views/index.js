a597188ba6b137a22df574fff9e5e4f286c5fb07
章智 <xyazyx@163.com>
2015-07-15 19:22:34 +0800

    改变参数

--- app/views/index.js
+++ app/views/index.js
@@ -7,27 +7,45 @@ window.PG.setConfig({
 	debug:true
 });
 
+var obj = {
+	partner:"2088101568358171",
+	seller_id:"xxx@alipay.com",
+	out_trade_no:"0819145412-6177",
+	subject:"测试",
+	body:"测试测试",
+	total_fee:"0.0.1",
+	notify_url:"http://notify.msp.hk/notify.htm?orderId=123456789",
+	service:"mobile.securitypay.pay",
+	payment_type:"1",
+	_input_charset:"utf-8",
+	it_b_pay:"30m",
+	sign:"lBBK%2F0w5LOajrMrji7DUgEqNjIhQbidR13GovA5r3TgIbNqv231yC1NksLdw%2Ba3JnfHXoXuet6XNNHtn7VE%2BeCoRO1O%2BR1KugLrQEZMtG5jmJIe2pbjm%2F3kb%2FuGkpG%2BwYQYI51%2BhA3YBbvZHVQBYveBqK%2Bh8mUyb7GM1HxWs9k4%3D",
+	sign_type:"RSA"
+};
+
+var str = "",
+	arr = [];
+
+for(var key in obj){
+	arr.push(key+'='+obj[key]);
+}
+
+str = arr.join('&');
+
 window.PG.ready().then(function(interfaces){
 
 	$('#aliPay').click(function(){
 		interfaces.aliPay({
-			seller:'2088901408530365',
-			partner:'2088901408530365',
-			tradeNO:'13223424234',
-			productName:'productName',
-			productDescription:'商品描述',
-			amount:'20',
-			notifyURL:'www.baidu.com',
-			service:'mobile.securitypay.pay',
-			paymentType:'1',
-			inputCharset:'utf-8',
-			itBPay:'30m',
-			showUrl:'m.alipay.com',
-			privateKey:'MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAKyzAlEDI/KACtXQH6F5/zR/L6IFh0REDtmWQ5+nXS1m57q6aBEkhbi0kM3iJnwdfoHNcbAyaj9a7G2XOBsSVfBOpYMBcrf/uyvpJEKr7W2b06DxyJZOvZrCa+e2iEHK7qFW/wGpqdpnzXS6ZfqRZyZDVCnG8mfQDM92+pobkzxdAgMBAAECgYBGQhVBLAvlzh2aWq9jnCQtEUkmyIqrZ10++LLAuWvYNwJZWiNANS+DgeTGtwZRk5/NDt6OShsWesxsoGK0ezPyTZ6tFlG5WhUBkURpzoHaPjtaZfxTH7z8ccTD6ENJuQfU1vknzAGAhPgog3FtmQ473971XdCrtfXxhYb72xiKQJBAOMv2nxZdFptpc+x6oZPaIwpJZ6tsfIdbgiMtKCu8LtTOoH16/r/GQpOcxMnI3fj9//9As0jeexY4XfVCHUrAcsCQQDCmhZ9Vzi2Yc7a7eFaJDnXdFgBMJBmiD47he8s7iURDePZ3O+09aMPtFPOUgbRhQfb6DiJq1z622fmeLgjV1V3AkBzhOWnzSSoxpBFZ8ZEvSQAAPwsD04oOma02jcObWXjGpLpek6ftg7ZesJwvrn+QGDePZyrCpr4Xslm0WAXk+K5AkEAgaaFW0aUGza4yTV5UUQXYUs7IrfHTPhydde6zLVw7pbReiHvdiG9XMYom6ocrtSxsrpZiKcp0FwDMhK1o54Y6wJAcWra/CHrJ+Pe/qT0K6vg+6lHGEkLs4lsm3zO2VIV6X+QrYEoJEYFUSiX6aFOGqNXkmK2DZ6STRhvoHhr8GH7Ag=='
-
+			packageValue:str
 		}).then(function(res){
 			alert(JSON.stringify(res));
 			PG.log(JSON.stringify(res));
 		});
 	});
 });
+
+
+
+
+
+
